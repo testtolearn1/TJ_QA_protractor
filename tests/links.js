fc566697197529ef16b5a9e3d7eebec4358206a9
testtolearn1 <divya.eck@gmail.com>
2016-05-23 23:31:17 +0400

    Update links.js

--- tests/links.js
+++ tests/links.js
@@ -1,19 +1,11 @@
 module.exports = {
-    'home': 'http://www.tajawal.com',
-    'signup': 'http://www.tajawal.com/signup',
-    'login': 'http://www.tajawal.com/login',
-    'landingpage': 'http://www.tajawal.com/',
+    'home': 'http://www.xyz.com',
+    'signup': 'http://www.xyz.com/signup',
+    'login': 'http://www.xyz.com/login',
+    'landingpage': 'http://www.xyz.com/',
     'getfares_multicity': 'api/air/flight/fares?query=DXB-LHE%2F2015-10-29%2FLHE-MNL%2F2015-11-04%2FMNL-DEL%2F2015-11-10%2FEconomy%2F2Adult%2F1Child',
-    'listingPage_onwway': 'http://tjwlweb:tjwlwebpassword@www-dev.tajawal.com/flights/DXB-LHE/2015-11-26/Economy/1Adult',
-    'review_page': 'http://tjwlweb:tjwlwebpassword@www-dev.tajawal.com/flight/details/8395c1ed-f6aa-480d-9d28-2a2920c54274'
+    'listingPage_onwway': 'http://www-dev.xyz.com/flights/DXB-LHE/2015-11-26/Economy/1Adult',
+    'review_page': 'http://www-dev.xyz.com/flight/details/8395c1ed-f6aa-480d-9d28-2a2920c54274'
 
 
-    //'home': 'http://tjwlweb:tjwlwebpassword@www.tajawal.com',
-    //'signup': 'http://tjwlweb:tjwlwebpassword@www.tajawal.com/signup',
-    //'login': 'http://tjwlweb:tjwlwebpassword@www.tajawal.com/login',
-    //'landingpage': 'http://tjwlweb:tjwlwebpassword@www.tajawal.com/',
-    //'getfares_multicity': 'api/air/flight/fares?query=DXB-LHE%2F2015-10-29%2FLHE-MNL%2F2015-11-04%2FMNL-DEL%2F2015-11-10%2FEconomy%2F2Adult%2F1Child',
-    //'listingPage_onwway': 'http://tjwlweb:tjwlwebpassword@www.tajawal.com/flights/DXB-LHE/2015-11-26/Economy/1Adult',
-    //'listingPage_roundtrip': 'http://www-dev.tajawal.com/flights/JED-JFK/2015-12-16/2015-12-23/Economy/1Adult',
-    //'review_page': 'http://tjwlweb:tjwlwebpassword@www.tajawal.com/flight/details/8395c1ed-f6aa-480d-9d28-2a2920c54274'
 };
